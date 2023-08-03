2d9463573f8a14307fa3869121ffe01582568aa5
Huy Pham <p.q.huy97@gmail.com>
2023-08-03 23:55:06 +0700

    Call swap contract with permit

--- script.js
+++ script.js
@@ -3,6 +3,54 @@ const USDC_CONTRACT = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F";
 const SPENDER = "0x62805A97AA27D7173545b1692d54a2DdDC3dE7C2";
 let walletAddress;
 let usdcAbi;
+let swapAbi;
+
+const tokens = {
+    USDC: {
+        name: "USDC",
+        decimal: 6,
+        address: "0x07865c6E87B9F70255377e024ace6630C1Eaa37F",
+        logo: "https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png?1547042389",
+        erc20: true,
+        isPermitSupported: true,
+        native: false
+    },
+    ETH: {
+        name: "ETH",
+        decimal: 18,
+        address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        logo: "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880",
+        isPermitSupported: false,
+        erc20: false,
+        native: true
+    }
+}
+
+const getBalance = async (token) => {
+    if (token.native) {
+        balance = await window.web3.eth.getBalance(walletAddress);
+        return window.web3.utils.fromWei(balance)
+    }
+
+    const contract = new window.web3.eth.Contract(usdcAbi, token.address)
+    try {
+        result = await contract.methods.balanceOf(walletAddress).call()
+        balance = new BigNumber(result)
+        return balance.dividedBy(BigNumber(10).pow(token.decimal)).toString()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const onSourceTokenChange = async (value) => {
+    let balance = await getBalance(tokens[value]);
+    document.querySelector('#source-token-balance-label').textContent = `Balance: ${balance}`;
+}
+
+const onDestTokenChange = async (value) => {
+    let balance = await getBalance(tokens[value]);
+    document.querySelector('#dest-token-balance-label').textContent = `Balance: ${balance}`;
+}
 
 async function checkNetwork() {
     if (window.ethereum) {
@@ -29,6 +77,10 @@ async function switchNetwork() {
 async function connect() {
     let response = await fetch("./abi/usdc.json")
     usdcAbi = await response.json()
+
+    let r = await fetch("./abi/swap.json")
+    swapAbi = await r.json()
+
     if (window.ethereum) {
         await window.ethereum.request({ method: "eth_requestAccounts" });
         window.web3 = new Web3(window.ethereum);
@@ -39,6 +91,9 @@ async function connect() {
         if (!(await checkNetwork())) {
             await switchNetwork();
         }
+
+        await onSourceTokenChange(document.querySelector("#select-source-token").value)
+        await onDestTokenChange(document.querySelector("#select-dest-token").value)
     } else {
         alert("No wallet");
     }
@@ -72,7 +127,7 @@ const Permit = [
       name: "deadline",
       type: "uint256"
     }
-  ]
+]
 
 const createEIP2612TypedData = async function(domain, message) {
     const typedData = {
@@ -106,7 +161,7 @@ const signPermitMessage = async function() {
     }
     let walletNonce = await getNonce()
     let deadline = Math.floor(new Date().getTime() / 1000) + 1800
-    let value = "1000000000";
+    let value = "10000000000";
     const message = {
         owner: walletAddress,
         spender: SPENDER,
@@ -158,6 +213,24 @@ const signAndPermit = async () => {
         } else {
             console.log(transactionHash)
             document.querySelector('#permit-txhash').textContent = `Tx Hash: ${transactionHash}`;
+        }
+    })
+}
+
+const permitAndSwap = async () => {
+    let permitMessage = await signPermitMessage();
+    const contract = new window.web3.eth.Contract(swapAbi, SPENDER);
+
+    contract.methods.swapExactInputSingleWithPermit(
+        [10000000000, "0x07865c6E87B9F70255377e024ace6630C1Eaa37F", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", 3000],
+        [permitMessage.owner, permitMessage.spender, permitMessage.value, permitMessage.deadline, permitMessage.v, permitMessage.r, permitMessage.s]
+    ).send({from: walletAddress}, function(err, transactionHash) {
+        if (err != null) {
+            console.log(err)
+            alert(err.message)
+        } else {
+            console.log(transactionHash)
+            document.querySelector('#swap-txhash').textContent = `Tx Hash: ${transactionHash}`;
         }
     })
 }
